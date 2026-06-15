b9653bf318f5c2e6fba57494318941b779ca5f10
Oziel Velazquez <ozielvelazquez24@outlook.com>
2026-06-15 14:22:59 -0600

    Version inicial de Cafe Andromeda

---
+++ cafe_andromeda.py
@@ -0,0 +1,8 @@
+# Programa Cafe Andromeda v1.0
+# Esta primera version del programa pedira al usuario:
+# Nombre del producto
+# Cantidad
+# Precio unitario
+# Al final le mostrara un ticket detallado con el subtotal, I.V.A. y total a pagar.
+
+# Declaracion de variables
